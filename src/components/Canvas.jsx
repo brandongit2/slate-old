cae7dee8b317bbe8f208243987b0f07c656aea61
Brandon Tsang <brandononline2@gmail.com>
2018-11-18 10:48:05 -0500

    Committed things that were omitted from the previous commit.

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -11,9 +11,14 @@ export class Canvas extends React.Component {
 
         this.svg = new SvgElement(this.props.width, this.props.height);
 
-        let link = new SVG.Link('https://google.com');
-        link.add(new SVG.Rect(200, 200, 'red'));
-        this.svg.add(link);
+        this.svg.add(new SVG.Rect(20, 20, 100, 100, 'red', 0, 0, 'black', 3));
+        this.svg.add(new SVG.Rect(140, 20, 100, 100, 'orange', 10, 10, 'black', 3));
+        this.svg.add(new SVG.Circle(310, 70, 50, 'yellow', 'black', 3));
+        this.svg.add(new SVG.Ellipse(410, 70, 30, 50, 'green', 'black', 3));
+        this.svg.add(new SVG.Line(20, 140, 120, 240, 'blue', 3));
+        this.svg.add(new SVG.PolyLine([[140, 140], [240, 240], [240, 140], [140, 240]], 'purple', 3));
+        this.svg.add(new SVG.Polygon([[293, 140], [326, 140], [360, 173], [360, 206], [326, 240], [293, 240], [260, 206], [260, 173]], 'grey', 'black', 3));
+        this.svg.add(new SVG.Path('M 380 140 v 100 q 100 0, 100 -100', 'black', 3));
     }
 
     componentDidUpdate() {
